23c19538a503e3de0dcdc199fac6c9a8936229c5
gv4383 <gregory.vargas18@gmail.com>
2022-01-02 01:16:37 -0600

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Goyo Vargas on 1/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
