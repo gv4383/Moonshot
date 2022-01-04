847ef763eabfc12a191b776158d10b37a352646d
gv4383 <gregory.vargas18@gmail.com>
2022-01-03 22:38:47 -0600

    Add assets, json, and initial decode logic

---
+++ Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,28 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Goyo Vargas on 1/3/22.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [String: Astronaut] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return loaded
+    }
+}
